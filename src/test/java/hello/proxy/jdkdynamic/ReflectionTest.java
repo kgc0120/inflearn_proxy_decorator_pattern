e9a48a88e7d231e32430d834a720f6dcc07b0d75
GyuCheol <senru0508@gmail.com>
2022-09-18 10:31:52 +0900

    reflection

---
+++ src/test/java/hello/proxy/jdkdynamic/ReflectionTest.java
@@ -0,0 +1,82 @@
+package hello.proxy.jdkdynamic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author bumblebee
+ */
+@Slf4j
+public class ReflectionTest {
+
+    @Test
+    void reflection0() {
+        Hello target = new Hello();
+
+        // 공통 로직1 시작
+        log.info("start");
+        String result1 = target.callA(); // 호출하는 메서드가 다름
+        log.info("result ={}", result1);
+        // 공통 로직1 종료
+
+        // 공통 로직2 시작
+        log.info("start");
+        String result2 = target.callB(); // 호출하는 메서드가 다름
+        log.info("result ={}", result2);
+        // 공통 로직2 종료
+    }
+
+    @Test
+    void reflection1() throws Exception {
+        // 클래스 정보
+        Class classHello = Class.forName("hello.proxy.jdkdynamic.ReflectionTest$Hello"); // 내부 클래스는 $를 사용
+
+        Hello target = new Hello();
+        //callA 메서드 정보
+        Method methodCallA = classHello.getMethod("callA");
+        Object result1 = methodCallA.invoke(target); // callA라는 메서드를 invoke를 이용해서 호출하는데 어떤 인스턴스? target 이라는 인스터스
+
+        log.info("result1={}", result1);
+
+        //callB 메서드 정보
+        Method methodCallB = classHello.getMethod("callB");
+        Object result2 = methodCallB.invoke(target); // callA라는 메서드를 invoke를 이용해서 호출하는데 어떤 인스턴스? target 이라는 인스터스
+
+        log.info("result2={}", result2);
+    }
+
+    @Test
+    void reflection2() throws Exception {
+        // 클래스 정보
+        Class classHello = Class.forName("hello.proxy.jdkdynamic.ReflectionTest$Hello"); // 내부 클래스는 $를 사용
+
+        Hello target = new Hello();
+        Method methodCallA = classHello.getMethod("callA");
+        dynamicCall(methodCallA, target);
+
+        Method methodCallB = classHello.getMethod("callB");
+        dynamicCall(methodCallB, target);
+    }
+
+    private void dynamicCall(Method method, Object target) throws Exception{
+        log.info("start");
+        Object result = method.invoke(target);
+        log.info("result ={}", result);
+    }
+
+    @Slf4j
+    static class Hello {
+        public String callA() {
+            log.info("callA");
+            return "A";
+        }
+
+        public String callB() {
+            log.info("callB");
+            return "B";
+        }
+    }
+
+}
--- src/test/java/hello/proxy/pureproxy/decorator/code/MessageDecorator.java
+++ src/test/java/hello/proxy/pureproxy/decorator/code/MessageDecorator.java
@@ -19,7 +19,7 @@ public class MessageDecorator implements Component{
         log.info("MessageDecorator 실행");
 
         String result = component.operation();
-        String decoResult = "*****" + result + "*****";
+        String decoResult = "<< =====" + result + " ===== >>";
         log.info("MessageDecorator 꾸미기 적용 전={}, 적용 후 ={}",result, decoResult);
         return decoResult;
     }
