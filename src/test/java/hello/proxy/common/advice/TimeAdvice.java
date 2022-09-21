92cc56f4e696dec1e7d49db28ba78ac4bba13ea7
GyuCheol <senru0508@gmail.com>
2022-09-21 21:13:04 +0900

    proxy factory

---
+++ src/test/java/hello/proxy/common/advice/TimeAdvice.java
@@ -0,0 +1,25 @@
+package hello.proxy.common.advice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+/**
+ * @author bumblebee
+ */
+@Slf4j
+public class TimeAdvice implements MethodInterceptor {
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        log.info("TimeProxy 실행");
+        long startTime = System.currentTimeMillis();
+
+//        Object result = method.invoke(target, args);
+        Object result = invocation.proceed(); // target을 찾아서 인수를 넘겨서 실행시켜준다.
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("TimeProxy 종료 resultTime={}", resultTime);
+        return result;
+    }
+}
