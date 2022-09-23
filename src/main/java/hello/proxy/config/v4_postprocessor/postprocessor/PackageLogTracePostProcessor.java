fdaf93376b687754659cec519ccf64d073d29e1a
GyuCheol <senru0508@gmail.com>
2022-09-23 20:46:05 +0900

    bean postprocessor 적용

---
+++ src/main/java/hello/proxy/config/v4_postprocessor/postprocessor/PackageLogTracePostProcessor.java
@@ -0,0 +1,42 @@
+package hello.proxy.config.v4_postprocessor.postprocessor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+/**
+ * @author bumblebee
+ */
+@Slf4j
+public class PackageLogTracePostProcessor implements BeanPostProcessor {
+
+    private final String basePackage;
+    private final Advisor advisor;
+
+    public PackageLogTracePostProcessor(String basePackage, Advisor advisor) {
+        this.basePackage = basePackage;
+        this.advisor = advisor;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        log.info("param beanBame={}, bean={}", beanName, bean.getClass());
+
+        //프로시 적용 대상 여루 체크
+        //프로식 적용 대상이 아니면 원본을 그대로 진행
+        String packageName = bean.getClass().getPackageName();
+        if (!packageName.startsWith(basePackage)) {
+            return bean;
+        }
+
+        // 프록시 대상이면 프로시를 만들어서 반환
+        ProxyFactory proxyFactory = new ProxyFactory(bean);
+        proxyFactory.addAdvisor(advisor);
+
+        Object proxy = proxyFactory.getProxy();
+        log.info("creata proxy: target={}, proxy={}", bean.getClass(), proxy.getClass());
+        return proxy;
+    }
+}
