ff474cd06670d6d712e1bc74a6ed88a26228e0f2
GyuCheol <senru0508@gmail.com>
2022-09-24 17:45:41 +0900

    aop annotation

---
+++ src/main/java/hello/proxy/config/v6_aop/aspect/LogTraceAspect.java
@@ -0,0 +1,46 @@
+package hello.proxy.config.v6_aop.aspect;
+
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.aop.ThrowsAdvice;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author bumblebee
+ */
+@Slf4j
+@Aspect
+public class LogTraceAspect {
+
+    private final LogTrace logTrace;
+
+    public LogTraceAspect(LogTrace logTrace) {
+        this.logTrace = logTrace;
+    }
+
+    //advisor
+    @Around("execution(* hello.proxy.app..*(..))") // pointcout
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        // advice
+        TraceStatus status = null;
+        try{
+            String message = joinPoint.getSignature().toShortString();
+            status = logTrace.begin(message);
+
+            // 로직 호출
+            Object result = joinPoint.proceed();
+
+            logTrace.end(status);
+            return result;
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+
+}
